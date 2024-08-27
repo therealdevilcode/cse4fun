e20f072b5f48164dbc5659d13e32af0268eeb569
therealdevilcode <commonmanworld61@gmail.com>
2024-08-27 19:52:48 +0530

    Add files via upload

--- best.js
+++ best.js
@@ -1,7 +1,11 @@
 const students = [
     { name: "DBMS", result: "TUESDAY 2ND&3RD PERIOD,WEDNESDAY 1ST PERIOD,THURSDAY 1ST PERIOD, FRIDAY 4TH PERIOD" },
-    { name: "SE", result: "TUESDAY" },
-    { name: "DE", result: "SUPPLY FEE READY CHESKO BRO🥲 MANAM PASS AVVALEM" }
+    { name: "SE", result: "TUESDAY 3RD PERIOD,THURSDAY 4TH PERIOD, FRIDAY 2ND PERIOD" },
+    { name: "DE", result: "SUPPLY FEE READY CHESKO BRO🥲 MANAM PASS AVVALEM" },
+    { name: "ES", result: "GO STRIGHT AND TAKE LEFT LIBRARY VASTHADI NEAT GA BUNK KOTTU BRO🥲" },
+    { name: "EDC", result: "MONDAY 4TH PERIOD,TUESDAY 5TH PRIOD,THURSDAY 2ND PERIOD," },
+    { name: "labs", result: "monday morning python lab(people who have NAGA-DHOSAM THEY MAY AVOID) remaiuning meeku thelsu" },
+    { name: "Dj", result: "owner of the website" },
     
 ];
 
